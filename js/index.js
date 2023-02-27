21bba02a593ff3622650a36ee57e7fbf2f74c426
Jumanazar04 <yusupovjumanazar004@gmail.com>
2023-02-27 18:43:36 +0500

    nma gap

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-const johnWeight = +prompt('Johnning vazni')
-const johnHeight = +prompt('Johnning boyi')
-const bobWeight = +prompt('Bobning vazni')
-const bobHeight = +prompt('Bobning boyi')
+// const johnWeight = +prompt('Johnning vazni')
+// const johnHeight = +prompt('Johnning boyi')
+// const bobWeight = +prompt('Bobning vazni')
+// const bobHeight = +prompt('Bobning boyi')
 
-const johnBMI = Math.round(johnWeight / johnHeight ** 2)
-const bobBMI = Math.round(bobWeight / bobHeight ** 2)
+// const johnBMI = Math.round(johnWeight / johnHeight ** 2)
+// const bobBMI = Math.round(bobWeight / bobHeight ** 2)
 
-if  (johnBMI > bobBMI) {
-    console.log('Johnning BMI katta')
-}else if (johnBMI === bobBMI) {
-    console.log('Ikkalasining BMI teng')
-}else  {
-    console.log('Bobning BMI katta')
-}
+// if  (johnBMI > bobBMI) {
+//     console.log('Johnning BMI katta')
+// }else if (johnBMI === bobBMI) {
+//     console.log('Ikkalasining BMI teng')
+// }else  {
+//     console.log('Bobning BMI katta')
+// }
 
-console.log(johnBMI)
-console.log(bobBMI)
+// console.log(johnBMI)
+// console.log(bobBMI)
 
 
 
